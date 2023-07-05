c77ee493efb50a413d30b2bec1eb358fc028b10e
grandeljay <github.jay@grandel.anonaddy.me>
2023-07-05 13:53:02 +0200

    chore(module): update to v0.2.1

--- src/includes/modules/shipping/grandeljayshippinglabel.php
+++ src/includes/modules/shipping/grandeljayshippinglabel.php
@@ -16,7 +16,7 @@ use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 class grandeljayshippinglabel extends StdModule
 {
-    public const VERSION = '0.1.1';
+    public const VERSION = '0.2.1';
 
     /**
      * Keys to automatically add on __construct and to remove on remove.
